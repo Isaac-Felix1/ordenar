0d38ada6fb09913fa6e84b58b18131a267090424
Isaac-Felix1 <isaacgabrielfelix1996@outlook.com>
2024-07-04 16:49:38 -0500

    first commit

--- R/hello.R
+++ R/hello.R
@@ -1,24 +1,21 @@
-ordenar_digitos_burbuja <- function(numero) {
-  # Convertir el número a caracteres y luego a un vector de caracteres
-  digitos <- as.character(numero)
-
-  # Longitud del vector de dígitos
-  n <- nchar(numero)
-
-  # Aplicar el algoritmo de la burbuja
-  for (i in 1:(n - 1)) {
-    for (j in 1:(n - i)) {
-      # Convertir caracteres a números para comparar
-      if (as.numeric(digitos[j]) > as.numeric(digitos[j + 1])) {
-        # Intercambiar dígitos si el actual es mayor que el siguiente
-        temp <- digitos[j]
-        digitos[j] <- digitos[j + 1]
-        digitos[j + 1] <- temp
-      }
-    }
-  }
-  # Convertir el vector de caracteres ordenado de nuevo a número
-  numero_ordenado <- as.numeric(paste(digitos, collapse = ""))
-
-  return(numero_ordenado)
+# Función para calcular el factorial de un número
+factorial <- function(n) {
+  return(ifelse(n == 0, 1, n * factorial(n - 1)))
 }
+
+# Función para calcular el número recursivo de un número
+recursivo <- function(n) {
+  return(ifelse(n == 0, 1, ifelse(n == 1, 0, (n - 1) * (recursivo(n - 1) + recursivo(n - 2)))))
+}
+
+# Función para verificar y mostrar si un número es recursivo
+es_recursivo <- function(numero) {
+  resultado_factorial <- factorial(numero)
+  resultado_recursivo <- recursivo(numero)
+
+  mensaje <- ifelse(resultado_recursivo == resultado_factorial, paste(numero, "es un número recursivo."), paste(numero, "NO es un número recursivo."))
+
+  cat(mensaje, "\n")
+}
+
+
